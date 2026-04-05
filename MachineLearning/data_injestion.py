93c7bab60f823ea406beadf6695871ca31c98def
SivaKavindraTamilSelvan <sivakavindra@gmail.com>
2026-04-05 18:18:50 +0530

    load raw data function created to store the duplicate of original data

--- MachineLearning/data_injestion.py
+++ MachineLearning/data_injestion.py
@@ -10,5 +10,17 @@ OUT_PATH = os.path.join(OUT_DIR, "raw_data.csv") #add this to the folder
 #the reason to crete a raw.data.csv is to protect the original data
 #if source file is gone or deleted or modified
 
+def load_raw_data(csv_path: str = "./Mall_Customer.csv") -> pd.DataFrame:
+    if os.path.exists(csv_path):
+        df = pd.read_csv(csv_path)
+        print(f"  Loaded from file : {csv_path}")
+    else:
+        raise FileNotFoundError(f"CSV not found at: {csv_path}")
+    return df
+
+
 if __name__ == "__main__":
     print("  MODULE 1 — DATA INGESTION")
+
+    df = load_raw_data(csv_path="./Mall_Customers.csv")
+    df.to_csv(OUT_PATH, index=False)
