92f18824adaaacdd6da8a8c473805b4232586bab
SivaKavindraTamilSelvan <sivakavindra@gmail.com>
2026-04-06 23:34:50 +0530

    Included every changes needed and testing is done

--- ORM/Practice/example.py
+++ ORM/Practice/example.py
@@ -8,12 +8,34 @@ class User(Model):
     name = CharField()
     age = IntegerField()
 
-User.create_table()
-User.get()
-
 class Book(Model):
     id = IntegerField(PRIMARY_KEY=True)
     author_name = ForeignKey(User)
     book_name=CharField()
 
-User.filter("<","age",26)
+
+User.create_table()
+Book.create_table()
+
+a=User(id=1,name='Siva Kavindra',age=20).save()
+b=User(id=2,name='Kavindra',age=30).save()
+c=User(id=3,name='Siva',age=10).save()
+
+d=Book(id=1,author_name=a,book_name='C-Fundamentals').save()
+e=Book(id=2,author_name=a,book_name='Java').save()
+f=Book(id=3,author_name=b,book_name='Java').save()
+
+User.get()
+User.get("age")
+
+User.filter("<","age",25).fetch()
+User.filter("=","age",25).fetch()
+User.filter("!=","age",20).fetch()
+
+User.order_by("age").fetch()
+User.filter("!=","age",20).order_by("age").fetch()
+
+User.delete(("age",20))
+User.get()
+
+Book.group_by("author_name")
